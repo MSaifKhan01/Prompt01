fabdd0d9d293b465852fa1269936b397ba38d5bf
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-08-04 15:40:20 +0530

    Prompt01

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors=require("cors")
 const OpenAIRouter = require("./Routes/openai.route")
 
 const app=express()
+
 app.use(cors())
+
 app.use(express.json())
 
 app.use("/openai",OpenAIRouter)
@@ -13,7 +15,7 @@ app.use("/openai",OpenAIRouter)
 app.listen(4001,async()=>{
     try {
 
-        
+
         console.log("connected")
     } catch (error) {
         console.log(error)
